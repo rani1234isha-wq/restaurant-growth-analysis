08f178c52e66d78947a29acfab0835295a3cf782
rani1234isha-wq <rani1234isha@gmail.com>
2026-06-22 18:51:29 +0530

    Add app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Restaurant Growth Analysis Dashboard")
+
+st.write("Restaurant Growth Analysis using Business Analytics")
+
+df = pd.read_csv("Final_Restaurant_Analysis.csv")
+
+st.subheader("Dataset Preview")
+st.dataframe(df.head())
+
+st.subheader("Summary Statistics")
+st.write(df.describe())
+
+st.success("Project deployed successfully using Streamlit")
